fffc8a6aef90bc6e879b4d86df6c092eccf8cea0
EduardoSantos <eduardofelipi@gmail.com>
2025-07-14 19:22:37 -0300

    Adicionando o resto do meu projeto

---
+++ app/src/main/java/com/edufelip/notetakingapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.edufelip.notetakingapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.databinding.DataBindingUtil
+import com.edufelip.notetakingapp.databinding.ActivityMainBinding
+import com.edufelip.notetakingapp.ui.theme.NoteTakingAppTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
