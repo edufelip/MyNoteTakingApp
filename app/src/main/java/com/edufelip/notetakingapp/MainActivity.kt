4e3a13907b9e5236e87d5b519719ae35b363a130
EduardoSantos <eduardofelipi@gmail.com>
2025-07-22 19:47:11 -0300

    Reverting changes to composable initial setup

--- app/src/main/java/com/edufelip/notetakingapp/MainActivity.kt
+++ app/src/main/java/com/edufelip/notetakingapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.edufelip.notetakingapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,17 +11,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.databinding.DataBindingUtil
-import com.edufelip.notetakingapp.databinding.ActivityMainBinding
 import com.edufelip.notetakingapp.ui.theme.NoteTakingAppTheme
 
 class MainActivity : ComponentActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContent {
+            NoteTakingAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NoteTakingAppTheme {
+        Greeting("Android")
     }
 }
