3bd685fb2fc4b85a29085f2c99c4f5ba680c61b6
EduardoSantos <eduardofelipi@gmail.com>
2025-07-25 21:17:00 -0300

    Reinitializing project with compose

--- app/src/main/java/com/edufelip/notetakingapp/MainActivity.kt
+++ app/src/main/java/com/edufelip/notetakingapp/MainActivity.kt
@@ -6,42 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.edufelip.notetakingapp.ui.theme.NoteTakingAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NoteTakingAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { /* TODO: Open add-note dialog */ }) {
+                            Icon(Icons.Default.Add, contentDescription = "Add Note")
+                        }
+                    },
+                    topBar = {
+                        TopAppBar(title = { Text("My Notes") })
+                    }
+                ) { innerPadding ->
+                    // Placeholder for notes list
+                    Text(
+                        "No notes yet!",
+                        modifier = Modifier.padding(innerPadding).padding(16.dp)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteTakingAppTheme {
-        Greeting("Android")
     }
 }
