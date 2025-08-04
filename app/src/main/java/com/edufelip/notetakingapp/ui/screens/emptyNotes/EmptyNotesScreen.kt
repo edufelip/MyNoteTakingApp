aa04e196067ebdfcbf230b93d5df8f61f8f52979
EduSantos <eduardofelipi@gmail.com>
2025-08-04 19:47:14 -0300

    feature/01: Removing unused imports

--- app/src/main/java/com/edufelip/notetakingapp/ui/screens/emptyNotes/EmptyNotesScreen.kt
+++ app/src/main/java/com/edufelip/notetakingapp/ui/screens/emptyNotes/EmptyNotesScreen.kt
@@ -17,8 +17,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
